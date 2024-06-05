287ff85a76d41d61855f0176c7414c633167b8bc
Kazik <Kazmierczak48913@gmail.com>
2024-06-05 21:24:31 +0200

    some tests with writing/saving file

--- dags/dag_polygon_ticker_types.py
+++ dags/dag_polygon_ticker_types.py
@@ -12,7 +12,7 @@ default_args = {
 }
 
 
-def extract_ticker_types_task(ti, **kwargs):
+def extract_ticker_types_task(**kwargs):
     import extract
     e = extract.ClientSync(kwargs["api_key"],
                            kwargs["host"])
@@ -27,23 +27,33 @@ def transform_ticker_types_task(ti):
     raw_json_file = ti.xcom_pull(task_ids='extract_ticker_types')
     raw_data = extract.read_json(raw_json_file)
     transformed_data = t.transform_ticker_types_response(raw_data)
-    meta = [
-        {"operation": "T"},
-        {"name": t.transform_ticker_types_response.__name__},
-        transformed_data.to_dict()
-    ]
-    return extract.write_json(meta)
+    directory = f"/opt/airflow/dags/data/T/transform_ticker_types_response/test.xml"
+    transformed_data.to_xml(directory)
+    return directory
+
+
+def load_ticker_types(ti):
+    import extract
+    import load
+    import pandas as pd
+    data_file_path = ti.xcom_pull(task_ids='transform_ticker_types')
+    data = pd.read_xml(data_file_path)
+    
+    mssql_hook = MsSqlHook(mssql_conn_id="mssql")
+    print(mssql_hook.default_conn_name)
+    
+    return data.describe()
     
 
 with DAG (
-    dag_id='dag_polygon_ticker_types_v21',
+    dag_id='dag_polygon_ticker_types_v32',
     description='fetch all ticker types from polygon.io',
     default_args=default_args,
     start_date=datetime(2024,6,3),
     schedule='@daily',
     catchup=False  
 ) as dag:
-    extract_ticker_types = PythonOperator(
+    task_1 = PythonOperator(
         task_id="extract_ticker_types",
         python_callable=extract_ticker_types_task,
         op_kwargs={
@@ -52,10 +62,15 @@ with DAG (
         }
     )
     
-    transform_ticker_types = PythonOperator(
+    task_2 = PythonOperator(
         task_id="transform_ticker_types",
         python_callable=transform_ticker_types_task
     )
     
+    task_3 = PythonOperator(
+        task_id="load_ticker_types",
+        python_callable=load_ticker_types
+    )
     
-    extract_ticker_types >> transform_ticker_types
+    
+    task_1 >> task_2 >> task_3
